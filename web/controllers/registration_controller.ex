ab6a456b561349dace718347e50103518fa30800
AudioStakes <audiostakes@gmail.com>
2016-10-30 18:03:49 +0900

    Add User Registration function

--- web/controllers/registration_controller.ex
+++ web/controllers/registration_controller.ex
@@ -11,4 +11,26 @@ defmodule HelloPhoenix.RegistrationController do
     # renderの第三引数に値を渡すことで、ビューやテンプレートで値を使用できる
     render conn, "new.html", changeset: changeset
   end
+
+  @doc """
+  ユーザ登録処理
+  """
+  def create(conn, %{"user" => user_params}) do
+    # フォーム情報user_paramsの値でuserデータを作成
+    changeset = User.changeset(%User{}, user_params)
+
+    # ユーザ登録
+    case User.create(changeset, HelloPhoenix.Repo) do
+      {:ok, user} ->
+        # バリデーションに成功した場合、userレコードを作成し、ログインし、"/"にリダイレクト
+        conn
+        |> put_flash(:info, "ようこそ" <> changeset.params["email"])
+        |> redirect(to: "/")
+      {:error, changeset} ->
+        # バリデーションに失敗した場合、"new.html"を表示
+        conn
+        |> put_flash(:info, "アカウントを作成できませんでした")
+        |> render("new.html", changeset: changeset)
+    end
+  end
 end
