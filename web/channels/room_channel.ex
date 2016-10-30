6fca664f43c89ff025ba9d3b12353fbf6bf55d2b
AudioStakes <audiostakes@gmail.com>
2016-10-30 17:05:15 +0900

    Add Transimit and Receive function of Messages

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,9 +1,16 @@
 defmodule HelloPhoenix.RoomChannel do
-  use Phoenix.Channel
+	use Phoenix.Channel
 
-  # "rooms:lobby"トピックのjoin関数
-  # {:ok, socket} を返すだけなのですべてのクライアントが接続可能
-  def join("rooms:lobby", message, socket) do
-    {:ok, socket}
-  end
+	# "rooms:lobby"トピックのjoin関数
+	# {:ok, socket} を返すだけなのですべてのクライアントが接続可能
+	def join("rooms:lobby", message, socket) do
+		{:ok, socket}
+	end
+
+	# イベント名"new:message"のIncoming eventsを処理する
+	def handle_in("new:message", message, socket) do
+		# broadcat!は同じチャネルのすべてのサブスクライバーにメッセージを送る
+		broadcast! socket, "new:message", %{user: message["user"], body: message["body"]}
+		{:noreply, socket}
+	end
 end
--- web/static/js/my_socket.js
+++ web/static/js/my_socket.js
@@ -19,6 +19,8 @@ class MySocket {
       if (e.keyCode === 13) { // 13: Enterキー
         // `${変数}` は式展開
         console.log(`[${this.$username.val()}]${this.$message.val()}`)
+        // サーバーに"new:messege"というイベント名で、ユーザ名とメッセージを送る
+        this.channel.push("new:message", { user: this.$username.val(), body: this.$message.val() })
         // メッセージの入力フィールドをクリア(空)にする
         this.$message.val("")
       }
@@ -44,6 +46,22 @@ class MySocket {
       .receive("error", resp => { // チャネルに入れなかった時の処理
         console.log("Unable to join", resp)
       })
+
+    // チャネルの"new:message"イベントを受け取った時のイベント処理
+    this.channel.on("new:message", message => this._renderMessage(message) )
+  }
+
+  // メッセージを画面に表示
+  _renderMessage(message) {
+    let user = this._sanitize(message.user || "New User")
+    let body = this._sanitize(message.body)
+
+    this.$messagesContainer.append(`<p><b>[${user}]</b>: ${body}</p>`)
+  }
+
+  // メッセージをサニタイズする
+  _sanitize(str) {
+    return $("<div/>").text(str).html()
   }
 }
 
